7aeb207e109fda0a0512b2786e24dd10ccc65a8b
PCdLf <peter_de_looff@hotmail.com>
2021-06-14 17:59:05 +0200

    Rmssd function to plot in report

---
+++ R/ibi_analysis_rmssd_time_period.R
@@ -0,0 +1,29 @@
+#' Calculation of RMSSD over 1 minute time periods for plotting
+#' @param IBIdata Uses the IBI data frame as created by \code{\link{read_e4}}
+#' @export
+#' @importFrom varian rmssd
+
+
+function (IBIdata)
+{
+  #heart rate variability from inter beat intervals
+  #https://www.sciencedirect.com/science/article/pii/S0735109797005548
+  #https://www.hrv4training.com/blog/heart-rate-variability-normal-values
+  
+  # Successive RR interval differences (in milliseconds)
+  IBI <- IBIdata %>% mutate(IBI_ms = (IBI * 1000))
+  
+  # Calculates the root mean square of successive differences (RMSSD)
+  RMSSD <- IBI %>%
+    #group per 5 minute interval
+    group_by(bin=cut(DateTime, "1 min")) %>%
+    #when more than one datapoint
+    filter(n() > 1) %>%
+    #calculate root mean squared error of difference scores
+    #rmssd = root(average(difference between this and previous IBI)square)
+    summarize(RMSSD = rmssd(IBI_ms)) %>%
+    #cast grouping to a timestamp
+    mutate(time = as.POSIXct(as.character(bin)))
+  
+  return(RMSSD)
+}
