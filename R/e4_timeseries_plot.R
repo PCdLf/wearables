ea6423a17060bf6104b957961870351629676b38
Remko Duursma <remkoduursma@gmail.com>
2021-06-15 11:44:47 +0200

    timeseries plot fixes

--- R/e4_timeseries_plot.R
+++ R/e4_timeseries_plot.R
@@ -6,6 +6,8 @@
 #' @param average_lines Do you want to add averages to the plot
 #' @param yaxis_ranges What Range do you want to give the y axis?
 #' @param colours The colours of the graph, defaults to blue, orange, red, green
+#' @importFrom dygraphs dygraph dyOptions dyHighlight dyUnzoom dyLimit
+#' @importFrom dygraphs dyAxis dyEvent dyShading
 #' @export
 e4_timeseries_plot <- function(data, 
                                main_title = "",
@@ -28,57 +30,57 @@ e4_timeseries_plot <- function(data,
     
     begin_time <- min(index(ts)) - 30*60
     
-    out <- dygraph(ts, main = main_title, group = "plot2", 
+    out <- dygraphs::dygraph(ts, main = main_title, group = "plot2", 
                    ylab = ylab_title, height = 150, width = 900) %>%   
-      dyHighlight(highlightCircleSize = 5) %>%
-      dyOptions(drawPoints = FALSE, 
+      dygraphs::dyHighlight(highlightCircleSize = 5) %>%
+      dygraphs::dyOptions(drawPoints = FALSE, 
                 drawXAxis = draw_x_axis,
                 connectSeparatedPoints = TRUE,
                 colors = color) %>%
-      dyUnzoom()
+      dygraphs::dyUnzoom()
     # dyAxis(name = "x", valueRange = c(begin_time, NULL))
     
     
     if(!is.null(y_line_type)){
       
       if(y_line_type == "mean"){
-        y_line_val <- mean(ts, na.rm = TRUE) 
+        y_line_val <- base::mean(ts, na.rm = TRUE) 
         label <- "Mean"
       } else {
         label <- NULL
       }
       
       out <- out %>%
-        dyLimit(y_line_val, label, strokePattern = "dashed", color = color)
+        dygraphs::dyLimit(y_line_val, label, strokePattern = "dashed", color = color)
       
     }
     
     if(!is.null(yaxis_range)){
       out <- out %>%
-        dyAxis("y", valueRange = yaxis_range)
+        dygraphs::dyAxis("y", valueRange = yaxis_range)
     }
     
     
     if(!is.null(events)){
       
       # 'Start' events
-      for(i in 1:nrow(events)){
+      for(i in seq_along(events)){
         
         txt <- ifelse(events_label, events$Text[i], "")
         
         out <- out %>%
-          dyEvent(events$Start[i], txt, labelLoc="bottom")
+          dygraphs::dyEvent(events$Start[i], txt, labelLoc="bottom")
         
       }
       
       
       # Shading events
-      eventsub <- filter(events, !is.na(Start) & !is.na(End))
+      eventsub <- dplyr::filter(events, !is.na(Start) & !is.na(End))
       if(nrow(eventsub)){
         for(i in 1:nrow(eventsub)){
           
           out <- out %>%
-            dyShading(from = eventsub$Start[i],
+            dygraphs::dyShading(from = eventsub$Start[i],
                       to = eventsub$End[i],
                       color = eventsub$Color[i])
           
@@ -118,7 +120,7 @@ e4_timeseries_plot <- function(data,
                y_line_val = series_options$MOVE$custom_y_val,
                yaxis_range = series_options$MOVE$yaxis_range,
                color = .cc$visualisation$move$color) %>%
-      dyRangeSelector()
+      dygraphs::dyRangeSelector()
   )
 }
 
