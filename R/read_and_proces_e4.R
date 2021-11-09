692bea7a194f7cfbb4c925fda55157df632773c1
Remko Duursma <remkoduursma@gmail.com>
2021-11-09 13:17:56 +0100

    add quality flags; more data summary stats

--- R/read_and_proces_e4.R
+++ R/read_and_proces_e4.R
@@ -6,6 +6,7 @@
 #' @return An object with processed data and analyses, object of class 'e4_analysis'.
 #' @rdname read_and_process_e4
 #' @importFrom utils write.csv2
+#' @importFrom padr thicken
 #' @export
 read_and_process_e4 <- function(zipfile, tz = Sys.timezone()){
   
@@ -15,6 +16,20 @@ read_and_process_e4 <- function(zipfile, tz = Sys.timezone()){
   process_e4(data)
   
 }
+
+
+# Join EDA binary classifier output to a dataset, based on rounded 5sec intervals.
+# Rename 'label' to 'quality_flag'
+join_eda_bin <- function(data, eda_bin){
+  
+  padr::thicken(data, interval = "5 sec") %>% 
+    left_join(eda_bin, by = c("DateTime_5_sec" = "id")) %>%
+    select(-DateTime_5_sec) %>%
+    rename(quality_flag = label)
+  
+}
+
+
 
 
 #' @rdname read_and_process_e4
@@ -37,14 +52,92 @@ process_e4 <- function(data){
   
   eda_feat <- compute_features2(eda_filt)
   flog.info("EDA Features computed")
-
+  
   eda_bin_pred <- predict_binary_classifier(eda_feat)
   eda_mc_pred  <- predict_multiclass_classifier(eda_feat)
-  flog.info("Model predictions generated.")
+  flog.info("Model predictions generated, artifacts classified.")
+  
+  # Add quality flags to data
+  eda_filt <- join_eda_bin(eda_filt, eda_bin_pred)
+  eda_peaks <- join_eda_bin(eda_peaks, eda_bin_pred)
+  
+  # Time range of the data
+  r <- range(eda_filt$DateTime)
+  time_range <- as.numeric(difftime(r[2], r[1], units = "min"))
+  
+  # Additional summaries
+  hr_summary <- list(
+    HR_mean = mean(data$HR$HR),
+    HR_median = median(data$HR$HR),
+    HR_min = min(data$HR$HR),
+    HR_max = max(data$HR$HR),
+    HR_sd = sd(data$HR$HR)
+  )
+  
+  temp_summary <- list(
+    TEMP_mean = mean(data$TEMP$TEMP),
+    TEMP_median = median(data$TEMP$TEMP),
+    TEMP_min = min(data$TEMP$TEMP),
+    TEMP_max = max(data$TEMP$TEMP),
+    TEMP_sd = sd(data$TEMP$TEMP)
+  )
+  
+  acc_summary <- list(
+    ACC_mean = mean(data$ACC$a),
+    ACC_median = median(data$ACC$a),
+    ACC_min = min(data$ACC$a),
+    ACC_max = max(data$ACC$a),
+    ACC_sd = sd(data$ACC$a)
+  )
+  
+  eda_clean <- dplyr::filter(eda_filt, quality_flag == 1)
+  
+  if(nrow(eda_clean) > 0){
+    eda_summary <- list(
+      EDA_clean_mean = mean(eda_clean$EDA),
+      EDA_clean_median = median(eda_clean$EDA),
+      EDA_clean_min = min(eda_clean$EDA),
+      EDA_clean_max = max(eda_clean$EDA),
+      EDA_clean_sd = sd(eda_clean$EDA)
+    )  
+  } else {
+    eda_summary <- list(
+      EDA_clean_mean = NA,
+      EDA_clean_median = NA,
+      EDA_clean_min = NA,
+      EDA_clean_max = NA,
+      EDA_clean_sd = NA
+    )
+  }
+  
+  pks_clean <- dplyr::filter(eda_peaks, quality_flag == 1)
+  
+  if(nrow(eda_clean) > 0){
+    peaks_summary <- list(
+      peaks_clean_sum = nrow(pks_clean),
+      peaks_clean_per_min =  nrow(pks_clean) / time_range,
+      peaks_clean_mean_auc = mean(pks_clean$AUC),
+      peaks_clean_mean_amp = mean(pks_clean$amp)
+    )  
+  } else {
+    peaks_summary <- list(
+      peaks_clean_sum = 0,
+      peaks_clean_mean_auc = NA,
+      peaks_clean_mean_amp = NA
+    )
+  }
+  
   
   structure(list(
     data = data,
     ibi = ibi,
+    data_summary = list(
+      ACC = acc_summary,
+      TEMP = temp_summary,
+      HR = hr_summary,
+      EDA = eda_summary,
+      peaks = peaks_summary
+    ),
     eda_peaks = eda_peaks,
     eda_bin = eda_bin_pred,
     eda_mc = eda_mc_pred
